2bc9c81244adb7ca273770da3022cac83df0337c
Akshay gohil <63612604+Akshaygohil1@users.noreply.github.com>
2020-11-08 18:20:05 +0530

    Add files via upload

---
+++ Basics/src/Application.java
@@ -0,0 +1,25 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		int myNumber = 1 ;  //int is 32 bit in java 
+		short myShort = 123; // short can hold 16 bit value
+		long myLong = 12345; //long can hold upto 64 bit  value 
+		
+		double myDouble = 123.456;
+		float myFloat = 123.4f;
+		
+		char myChar  = 'y';
+		boolean myBoolean = false;
+		
+		
+		
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myBoolean);
+	}
+
+}
